97eeda43eb38da041b9a215e3cc75c53dc56b0d3
Daniel Gebremichael <daniel.gebremichael@hotmail.com>
2019-11-11 10:33:09 +0000

    Changed names of methods

--- luas_due_time.py
+++ luas_due_time.py
@@ -2,19 +2,18 @@ from xml.dom import minidom
 from urllib.request import urlopen
 from time import sleep
 from itertools import cycle
-#import curses
 
 url1 = "http://luasforecasts.rpa.ie/xml/get.ashx?action=forecast&stop=bla&encrypt=false"
 #url2 = "http://api.openweathermap.org/data/2.5/weather?q=Dublin,ie&APPID=17fd8116fd28f1de0578f7e64117d95a&mode=xml&units=metric"
 
-def printDueMins(trams):
+def print_due_mins(trams):
     total = []
     for tram in trams:
         total.append(str(tram.attributes['destination'].value +" "+tram.attributes['dueMins'].value))
     return total
 
 
-def pbar():
+def get_due_mins():
     dom = minidom.parse(urlopen(url1)) # parse the data
     inbound = dom.getElementsByTagName('direction')[0].getElementsByTagName('tram')
     outbound= dom.getElementsByTagName('direction')[1].getElementsByTagName('tram')
@@ -22,9 +21,9 @@ def pbar():
 
 duration = 0
 for frame in cycle(r'-\|/-\|/'):
-    inbound,outbound =pbar()
-    in_b = printDueMins(inbound)
-    out_b = printDueMins(outbound)
+    inbound,outbound =get_due_mins()
+    in_b = print_due_mins(inbound)
+    out_b = print_due_mins(outbound)
     print('\r', frame,'INBOUND',frame, sep='', end='\t', flush=True)
     for count in range(len(in_b)-1):
         print(in_b[count],sep=' ', end='\t', flush=True)
