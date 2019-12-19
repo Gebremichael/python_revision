b386f884511d0f24171574f06f53019e1c3fa910
Gebremichael <daniel.gebremichael@hotmail.com>
2019-12-19 14:40:53 +0000

    Shorter solution 1 and improved solution 2

--- palindrome.py
+++ palindrome.py
@@ -1,23 +1,23 @@
-# Check if user input string is a palindrome
+# Check if user input is a palindrome
 
 # get string from user
-word = str(input("Enter a word: "))
+word = input("Enter a word: ")
 
 # Two possible solutions
 
 # Solution 1 (simple)
-if (word == word[::-1]):
-    print(f"SOLUTION 1\nThe word \"{word}\" is a palindrome")
-else:
-    print(f"SOLUTION 1\nThe word \"{word}\" is not a palindrome")
+print("SOLUTION 1\nThe word",word,['is not','is'][word == word[::-1]] ,"a palindrome")
 
 # Solution 2
-#for x in range(0,len(user_input_word):
+# Compares the letters, first with last until it reachs the centre
 word_count = 0
 for count, letter in enumerate(word,start=1):
+    if count > len(word)//2:
+        break
     if(letter == word[len(word) -count]):
         word_count+=1
-if (word_count == len(word)):
-    print(f"SOLUTION 2\nThe word \"{word}\" is a palindrome")
+
+if (word_count == len(word)//2):
+    print(f"SOLUTION 2\nThe word {word} is a palindrome")
 else:
-    print(f"SOLUTION 2\nThe word \"{word}\" is not a palindrome")
+    print(f"SOLUTION 2\nThe word {word} is not a palindrome")
