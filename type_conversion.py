c1cd80528368baee5fdc38a5f99350d688615a47
Daniel Gebremichael <daniel.gebremichael@hotmail.com>
2020-02-18 15:19:29 +0000

    Added type conversion notes

---
+++ type_conversion.py
@@ -0,0 +1,21 @@
+a= '10001'
+b,c= 'word', ''
+d ='10'
+    # string to int 
+print( int(a)  )
+    # string to float
+print( float(a)  )
+    # string to bool
+print( bool(b), bool(c)  )
+
+    # string to binary
+print( ' '.join(format(ord(x), 'b') for x in 'cat') )
+    # binary to int
+print( int(a,2)  )
+    # hex to int
+print( int(d,16)  )
+
+    # 
+print(   )
+    # 
+print(   )
